9ed314ee2c5aca8bb24b867dcaeca6f4032629b4
mark <mark.kors@gmail.com>
2021-10-07 12:22:10 +0200

    Add project files.

---
+++ GlobalTest/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GlobalTest
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        public static Class1 MyClass;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MyClass = new Class1("test");
+
+        }
+
+
+    }
+}
---
+++ GlobalTest/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalTest
+{
+    public class Class1
+    {
+
+        String naam;
+        public Class1(string n)
+        {
+            naam = n;
+
+        }
+
+        public string MyProperty
+        {
+            get { return naam; }
+            set { naam = value; }
+        }
+    }
+}
+
